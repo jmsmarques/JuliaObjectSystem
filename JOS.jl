0c23209fab7e818c36a7755a863f1dba080dd91a
Jorge Miguel Da Silva Marques <jms@student.dei.uc.pt>
2019-05-08 12:03:15 +0100

    started

--- JOS.jl
+++ JOS.jl
@@ -1,4 +1,12 @@
 #structs definition
+struct Object
+    name::Symbol
+    superclass::Vector{Symbol}
+    parameters::Vector
+    parametersvalue::Vector
+    Object(name, superclass, parameters) = new(name, superclass, parameters)
+end
+
 struct IntrospectableFunction
     name
     args
@@ -15,7 +23,8 @@ square = IntrospectableFunction(:square,:(x,), :(x*x), x->x*x)
 
 #functions
 function make_class(name::Symbol, superclass::Vector, slots::Vector{Symbol})
-
+   
+    return
 end
 
 function make_instance(name::String, x...)
