cf15b841b8bff0731b5ac35b9c622cabfb1889d5
fbasto00@gmail.com <fbasto00@gmail.com>
2019-05-15 10:53:42 +0100

    DefClass Macro merge completed

--- Tests.jl
+++ Tests.jl
@@ -6,11 +6,8 @@ C2 = make_class(:C2, [C1],[:d])
 c1 = make_instance(C1, :a=>2,:b=>3)
 c2 = make_instance(C2,:a=>3,:d=>6)
 #dump(c2)
-dump(c2.parametersvalue)
+#dump(c2.parametersvalue)
 set_slot!(c2,:d,4)
-dump(c2.parametersvalue)
-# dump(:(c2.class))
-# println("\n\n")
 # dump(:(Meta.parse("c2.d")))
 
 #:((f::Class).(x)) = get_slot(:f, :x)
@@ -23,11 +20,11 @@ dump(c2.parametersvalue)
 @defclass(C3, [C1,C2], d,e,i)
 @defclass(C4, [C1,C3], d,e,i)
 
+#get_slot(c2,:a)
+#get_slot(c2,:b)
 
-"""
-get_slot(c2,:a)
+#println("After get slot\n")
 #set_slot!(c1, :f, 2)
-@defgeneric foo(c)
-<<<<<<< Updated upstream
-@defmethod foo(c1::R1) = 1
-"""
+#@defgeneric foo(c)=#
+#@defmethod foo(c1::R1) = 1
+
