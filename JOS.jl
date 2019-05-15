77a193eadc360c95f2abae5756616f660f4d27e3
Jorge Miguel Da Silva Marques <jms@student.dei.uc.pt>
2019-05-15 10:47:29 +0100

    finished @defclass

--- JOS.jl
+++ JOS.jl
@@ -21,6 +21,7 @@ end
 struct GenericFunction
     name::Symbol
     args::Vector{Symbol}
+    type::Type
     methods::Vector{SpecializedMethod}
 end
 
@@ -53,6 +54,16 @@ function make_class(name::Symbol, superclass::Vector, slots::Vector{Symbol})
     return object
 end
 
+function make_class(name::Symbol, superclass::Vector, slots::Tuple{Symbol})
+    params::Vector{Symbol} = []
+
+    for i in slots
+        push!(params, i)
+    end
+
+    object = Metaclass(name, superclass, params)
+    return object
+end
 
 function verifypairs(pairs)
     param = Pair[]
@@ -106,8 +117,13 @@ end
 #end of functions
 
 #macros
-macro defclass(name, superclass, slotnames)
-    
+macro defclass(name, superclass, slotnames...)
+
+    show(name);println()
+    show(superclass);println()
+    show(slotnames);println()
+
+    return :( $(esc(name)) = make_class(($(esc(QuoteNode(name)))), $superclass, $slotnames))
 end
 
 #create generic method
@@ -125,19 +141,32 @@ macro defgeneric(x)
    end
    object = GenericFunction(name,args,spe_methods)
    push!(gen_functions,object)
+   
    return object
 end
 
 macro defmethod(x)
     println("Inside def method macro")
-    methodName = x.args[1].args[1] 
+    methodname = x.args[1].args[1] 
+    println(methodname)
+    
     dump(x)
-    for gen in gen_functions
-        if gen.name == methodName
-            println("Mehtod name ",methodName)
 
-            # object = SpecializedMethod()
-        end
-    end
+    object = SpecializedMethod(x.args[1].args[2].args[1], [], :(x.args[2].args[2]), x.args[2].args[2])
+    
+    dump(object)
+
+    object.nativefunction
+
+    return :(x.args[2].args[2])
+    # for gen in gen_functions
+    #     if gen.name == methodName
+    #         println("Method name ",methodName)
+
+    #         # object = SpecializedMethod()
+    #     end
+    # end
+
+
 end
 #end of macros
--- Tests.jl
+++ Tests.jl
@@ -9,6 +9,20 @@ c2 = make_instance(C2,:a=>3,:d=>6)
 dump(c2.parametersvalue)
 set_slot!(c2,:d,4)
 dump(c2.parametersvalue)
+# dump(:(c2.class))
+# println("\n\n")
+# dump(:(Meta.parse("c2.d")))
+
+#:((f::Class).(x)) = get_slot(:f, :x)
+#(f::Expr)(args...) = get_slot(args...)
+# (Expr()) = get_slot(:a, :b)
+
+# dump(:(c2.class))
+
+# c2.d
+@defclass(C3, [C1,C2], d,e,i)
+@defclass(C4, [C1,C3], d,e,i)
+
 
 """
 get_slot(c2,:a)
